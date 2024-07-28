a396c70d591be7337a595b1a420de9a766844913
Yogesh Malakar <yogeshmalakar071@gmail.com>
2024-07-28 15:33:47 +0530

    register&login page

--- app/component/FrontHeader.js
+++ app/component/FrontHeader.js
@@ -12,7 +12,6 @@ export default function FrontHeader() {
   return (
     <>
 
-
       <header className="shadow sticky z-50 top-0">
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
