841c98a2db8b99a8a83e87b4651f31497a434631
Sandeep Rathod <rsandeeprathod3@gmail.com>
2024-07-29 00:08:02 +0530

    frontNavigation

--- Frontend/app/auth/login/page.jsx
+++ Frontend/app/auth/login/page.jsx
@@ -1,40 +1,100 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 function page() {
-    return (
-        <>
-
-            <div className='w-full sm:h-screen flex justify-center items-center'>
-                <div className=' w-auto bg-blue-500 m-auto p-5 rounded-md py-12 px-8'>
-                    <h3 className='text-center text-white'>LOGIN</h3>
-                    <form action="">
-                        <div className='mt-2'>
-                            <label htmlFor="email" className='text-white block'>Enter Email:</label>
-                            <input type="text" id='email' className='form-control mt-1 p-2 rounded-md' placeholder='Enter email' />
-                        </div>
-                        <div className='mt-2'>
-                            <label htmlFor="password" className='text-white block'>Enter Password:</label>
-                            <input type="password" id='password' className='form-control mt-1 p-2 rounded-md' placeholder='Password' />
-                        </div>
-                        <div className='mt-2 flex items-center'>
-                            <input type="checkbox" className='form-check-input' id='check' />
-                            <label htmlFor="check" className='form-check-label text-white ml-2'>Remember me</label>
-                        </div>
-                        <div className="mt-3">
-                            <button className="p-2 w-full bg-blue-700 text-white rounded-md">Login</button>
-                        </div>
-                        <div className="text-center mt-4">
-                            <h5 className="text-white">Create an account</h5>
-                        </div>
-                        <div className="mt-3">
-                            <Link href="/auth/register" className="block p-2 w-full bg-white text-center text-black rounded-md">Sign-up</Link>
-                        </div>
-                    </form>
-                </div>
+  return (
+    <>
+      <div className="w-full sm:h-screen flex justify-center items-center">
+        <div className=" w-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-[26px]  p-3  py-12 px-8">
+          <h2 className="text-center text-white font-semibold">LOGIN</h2>
+          <form action="">
+            <div className="mt-2">
+              <label htmlFor="email" className="text-white block">
+                Enter Email:
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="form-control mt-1 p-2 rounded-md"
+                placeholder="Enter email"
+              />
             </div>
-        </>
-    )
+            <div className="mt-2">
+              <label htmlFor="password" className="text-white block">
+                Enter Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control mt-1 p-2 rounded-md"
+                placeholder="Password"
+              />
+            </div>
+            <div className="mt-2 flex items-center">
+              <input type="checkbox" className="form-check-input" id="check" />
+              <label
+                htmlFor="check"
+                className="form-check-label text-white ml-2"
+              >
+                Remember me
+              </label>
+              <Link href="/" className="text-white ml-4">
+                Forget password
+              </Link>
+            </div>
+            <div className="mt-1 flex items-center"></div>
+            <div className="mt-3">
+              <button className="bg-gradient-to-r dark:text-gray-300 from-blue-500 to-purple-500 shadow-lg mt-6 p-2 text-white rounded-lg w-full hover:scale-105 hover:from-purple-500 hover:to-blue-500 transition duration-300 ease-in-out">
+                Login
+              </button>
+            </div>
+            <div className="text-center mt-4">
+              <h5 className="text-white">Don't have an account?<Link
+                href="/auth/register"
+                className="group text-blue-400 transition-all duration-100 ease-in-out"
+              >
+                Register
+              </Link>
+ </h5>
+            </div>
+            <div className="mt-3 flex items-center justify-center flex-wrap">
+              
+              <button
+                href="#"
+                class="hover:scale-105 ease-in-out duration-300 shadow-lg p-2 rounded-lg m-1"
+              >
+                <img
+                  class="max-w-[25px]"
+                  src="https://ucarecdn.com/8f25a2ba-bdcf-4ff1-b596-088f330416ef/"
+                  alt="Google"
+                />
+              </button>
+              <button
+                href="#"
+                class="hover:scale-105 ease-in-out duration-300 shadow-lg p-2 rounded-lg m-1"
+              >
+                <img
+                  class="max-w-[25px]"
+                  src="https://ucarecdn.com/95eebb9c-85cf-4d12-942f-3c40d7044dc6/"
+                  alt="Linkedin"
+                />
+              </button>
+              <button
+              href="#"
+              class="hover:scale-105 ease-in-out duration-300 shadow-lg p-2 rounded-lg m-1"
+            >
+              <img
+                class="max-w-[25px]"
+                src="https://ucarecdn.com/6f56c0f1-c9c0-4d72-b44d-51a79ff38ea9/"
+                alt="Facebook"
+              />
+            </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default page
+export default page;
--- Frontend/app/component/FrontFooter.js
+++ Frontend/app/component/FrontFooter.js
@@ -1,10 +1,95 @@
 import React from 'react'
+import Link from 'next/link';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
 
 function FrontFooter() {
   return (
-    <div>
-        <h1>sandeep sandeep footer</h1>
+    <footer className="border border-t-8 border-teal-500">
+    <div className="w-full max-w-7xl mx-auto">
+      <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+        <div className="mt-5">
+          <Link href="/">
+            <span className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white">
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Blog-App
+              </span>
+              
+            </span>
+          </Link>
+        </div>
+        <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+          <div>
+            <h5 className="text-lg font-semibold">About</h5>
+            <ul>
+              <li>
+                <Link href="https://www.100jsprojects.com">
+                  100 JS Projects
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  Blog-App
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="text-lg font-semibold">Follow us</h5>
+            <ul>
+              <li>
+                <Link href="https://github.com/SandeepRathod3">
+                  Github
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  Discord
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="text-lg font-semibold">Legal</h5>
+            <ul>
+              <li>
+                <Link href="#">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  Terms &amp; Conditions
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr className="my-4" />
+      <div className="w-full sm:flex sm:items-center sm:justify-between">
+        <p className="text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} Sandeep Rathod
+        </p>
+        <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+          <Link href="#">
+            <BsFacebook />
+          </Link>
+          <Link href="#">
+            <BsInstagram />
+          </Link>
+          <Link href="#">
+            <BsTwitter />
+          </Link>
+          <Link href="https://github.com/SandeepRathod3">
+            <BsGithub />
+          </Link>
+          <Link href="#">
+            <BsDribbble />
+          </Link>
+        </div>
+      </div>
     </div>
+  </footer>
   )
 }
 
