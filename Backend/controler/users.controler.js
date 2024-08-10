a80b49c5d6f50fdfedfc9734bcb3abdd174c54d7
Yogesh Malakar <yogeshmalakar071@gmail.com>
2024-08-10 21:17:01 +0530

    add a file

--- Backend/controler/users.controler.js
+++ Backend/controler/users.controler.js
@@ -1,4 +1,7 @@
 const registerUser = async(req,res) => {
     const {userName,fullName,email,password} = req.body
+    if(
+        [userName,fullName,email,password].some((field) => field?.trim() === "")
+    )
 }
 export {registerUser}
